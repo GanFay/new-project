66b347fd994e851d7856d46fdcabba45a8ff9e35
GanFay <macsbiesiedin170109@gmail.com>
2026-07-15 13:17:22 +0300

    upd12

--- split-notify/cmd/main.go
+++ split-notify/cmd/main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "github.com/ganfay/split-proto"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewPingClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	ping, err := c.SayPing(ctx, &pb.PingRequest{Name: "default"})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", ping.GetMessage())
+}
